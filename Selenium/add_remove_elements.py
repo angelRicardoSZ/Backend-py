bb8837560a84e24ec77f492b9d764670948694ef
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-07 07:53:21 -0500

    Intro Selenium: Working with adding and removing elements on the website using try-except to handle the errors if the users want to remove more elements than there are

---
+++ Selenium/add_remove_elements.py
@@ -0,0 +1,42 @@
+import unittest
+from selenium import webdriver
+from time import sleep
+
+class NavigationTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://the-internet.herokuapp.com")
+        driver.find_element_by_link_text("Add/Remove Elements").click()
+        
+    def test_add_remove(self):
+        driver =self.driver
+        elements_added =int(input("How many elements will you add?: "))
+        elements_removed = int(input("How many elements will you remove?: "))
+        total_elements = elements_added - elements_removed
+        add_button = driver.find_element_by_xpath('//*[@id="content"]/div/button')
+        sleep(3)
+        for i in range(elements_added):
+            add_button.click()
+            
+        for i in range(elements_removed):
+            try:
+                #delete_button = driver.find_element_by_xpath('//*[@id="elements"]/button[6]')
+                delete_button = driver.find_element_by_class_name('added-manually')
+                delete_button.click()
+            except:
+                print("You´re trying to delete more elements than the existent")        
+                break
+        if total_elements >= 0:
+            print(f'There are {total_elements} elements on screen') 
+        else:
+            print('There 0 are elements on screen')
+            
+        sleep(3)
+    
+    def tearDown(self):
+        self.driver.close()
+        
+if __name__ == "__main__":
+	unittest.main(verbosity = 2)
