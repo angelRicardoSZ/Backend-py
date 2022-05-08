62cb9c9e93316afb961d63ffc7e9aa632f2b0c50
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-07 20:45:44 -0500

    Intro selenium: working with Data Driven Testing model

---
+++ Selenium/search_ddt.py
@@ -0,0 +1,56 @@
+import csv, unittest
+from email import message
+from ddt import ddt, data, unpack
+from selenium import webdriver
+
+def get_data(filename):
+    rows = []
+    data_file = open(filename,'r')
+    reader = csv.reader(data_file)
+    next(reader,None)
+    
+    for row in reader:
+        rows.append(row)
+    return rows
+        
+
+
+@ddt
+class SearchDDT(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://demo-store.seleniumacademy.com/")
+        
+    @data(*get_data('testdata.csv'))    
+    @unpack
+    def test_search_ddt(self,search_value, expected_count):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
+        search_field.send_keys(search_value)
+        search_field.submit()
+        
+        products = driver.find_elements_by_xpath('//h2[@class="product-name"]/a')
+        expected_count = int(expected_count)
+        print(f"Found {len(products)} products")
+        
+        if expected_count > 0:
+            self.assertEqual(expected_count, len(products))
+        else:
+            message = driver.find_element_by_class_name('note-msg')
+            self.assertEqual('Your search has not results', message)
+            
+        print(f"Found {len(products)} products")
+            
+        self.assertEqual(expected_count,len(products))    
+        
+
+
+    def tearDown(self):
+        self.driver.close()
+           
+if __name__ == "__main__":
+	unittest.main(verbosity = 2)
+ 
