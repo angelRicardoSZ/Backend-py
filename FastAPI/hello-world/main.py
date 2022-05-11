0946e59a24270d123fc7f650932df7bd4b11a1b3
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-11 08:01:31 -0500

    Fastapi: working with path validations

--- FastAPI/hello-world/main.py
+++ FastAPI/hello-world/main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query, Path
 app = FastAPI()
 
 # Models
@@ -12,17 +12,43 @@ class Person(BaseModel):
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
     
-    
-
-
-
 @app.get("/")
 def home():
     return {"Hello":"World"}
 
 # Request and response body
-
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
     
+    
+# Validaciones: Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: str = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required"
+        )
+): 
+    return {name: age}
+
+# Validaciones: Path Parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person ID",
+        description="This is the person ID. It's reqiuired and it's more than 0."
+        )
+): 
+    return {person_id: "It exists!"}
+
