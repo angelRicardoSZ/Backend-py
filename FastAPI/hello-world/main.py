0a5964933a2979bdb9cb84ebf4b880f2bf982794
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-11 20:50:04 -0500

    Working with validations

--- FastAPI/hello-world/main.py
+++ FastAPI/hello-world/main.py
@@ -1,16 +1,48 @@
+from email.policy import default
 from typing import Optional
+from enum import Enum
+
 from pydantic import BaseModel
+from pydantic import Field
+
 from fastapi import FastAPI
 from fastapi import Body, Query, Path
 app = FastAPI()
 
 # Models
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+    
+
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    last_name: str =  Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default = None)
     
 @app.get("/")
 def home():
@@ -52,3 +84,19 @@ def show_person(
 ): 
     return {person_id: "It exists!"}
 
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="PERSON_ID",
+        desciption="This is the person ID",
+        gt=0        
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
