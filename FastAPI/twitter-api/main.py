9267adb08bb8652a6aa873df97b26b4c7f7cbea3
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-18 22:23:48 -0500

    fastapi: creating twitter clone, initialization of the project and path operations created

---
+++ FastAPI/twitter-api/main.py
@@ -0,0 +1,132 @@
+# Python
+from uuid import UUID
+from datetime import date
+from datetime import datetime
+from typing import Optional, List
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+
+#fastapi
+from fastapi import FastAPI
+from fastapi import status
+
+app = FastAPI()
+
+
+# Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    
+    
+    first_name: str = Field(
+        ...,
+        min_length = 1,
+        max_length = 50
+    )
+    last_name: str = Field(
+        ...,
+        min_length = 1,
+        max_length = 50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    tweet_id: UUID =Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+        )
+    created_at: datetime =Field(default=datetime.now())
+    updated_at: Optional[datetime] =Field(default=None)
+    by: User = Field(...)
+
+
+# Path operations
+
+@app.get(path="/")
+def home():
+    return {"Twitter API":"working"}
+
+
+## Users
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register User",
+    tags= ["Users"]
+)
+def signup():
+    pass 
+
+
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login User",
+    tags= ["Users"]
+)
+def login():
+    pass 
+
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all users",
+    tags= ["Users"]
+)
+def show_all_users():
+    pass 
+
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags= ["Users"]
+)
+def show_a_user():
+    pass 
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags= ["Users"]
+)
+def delete_a_user():
+    pass 
+
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags= ["Users"]
+)
+def update_a_user():
+    pass 
+
+## Tweets
