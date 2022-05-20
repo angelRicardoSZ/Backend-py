01a7b2ec304bf53b57a16722f01f789efd509ee5
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-05-20 08:33:11 -0500

    Flask: introduction

---
+++ Flask/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, make_response, redirect, render_template
+app = Flask(__name__)
+todos = ["Todo1","Todo2","Todo3"]
+
+@app.route("/")
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect("/hello"))
+    response.set_cookie("user_ip",user_ip)
+    return response
+
+@app.route("/hello")
+def hello():
+    user_ip = request.cookies.get("user_ip")
+    context = {
+        "user_ip":user_ip,
+        "todos":todos
+    }
+    return render_template("hello.html", **context)
+
