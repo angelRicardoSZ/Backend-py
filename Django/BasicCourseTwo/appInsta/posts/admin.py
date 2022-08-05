81de6321c1718b6e1d86c95272b97daf15ba137e
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-08-05 12:45:28 -0500

    posts model created

--- Django/BasicCourseTwo/appInsta/posts/admin.py
+++ Django/BasicCourseTwo/appInsta/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from posts.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts admin model"""
+    list_display = ('pk', 'user','title', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('photo',)
+    list_filter = (
+                'created',
+                'modified'
+    )
+    search_fields = ('user__username','title',)
+    readonly_fields = ('created', 'modified')
--- Django/BasicCourseTwo/appInsta/posts/models.py
+++ Django/BasicCourseTwo/appInsta/posts/models.py
@@ -1,1 +1,36 @@
+"""Post models"""
+
+import profile
+from django.db import models
+from django.contrib.auth.models import User
+
+from users.models import Profile
+
+class Post(models.Model):
+    """Post model.
+
+    Args:
+        user: foreign key (users)
+        profile: foreign key (profile)
+        title: post title
+        photo: post photo
+        created: post creation date
+        modified: post modified date
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        """Returns title and username
+
+        Returns:
+            str: title
+            str: username
+        """
+        return '{} by @{}'.format(self.title, self.user.username)
 
