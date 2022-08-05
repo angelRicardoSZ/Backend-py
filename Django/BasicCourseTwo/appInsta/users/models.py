3769fd8c542286cabe0fe24ea333065e3b3f300f
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-08-05 10:30:14 -0500

    editing admin daschboard

--- Django/BasicCourseTwo/appInsta/users/models.py
+++ Django/BasicCourseTwo/appInsta/users/models.py
@@ -20,6 +20,8 @@ class Profile(models.Model):
     
     website = models.URLField(max_length=200, blank=True)
     
+    biography = models.TextField(blank=True)
+    
     phone_number = models.CharField(max_length=20, blank=True)
     
     picture = models.ImageField(upload_to='users/pictures', blank=True, null=True)
